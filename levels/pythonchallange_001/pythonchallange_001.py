4589bc49149b3925c6f228b47063b0b20cfcdf38
Gyorgy Nadudvari <reedcourty@gmail.com>
2011-07-23 10:08:25 +0200

    Added the solution of Level 1.

---
+++ levels/pythonchallange_001/pythonchallange_001.py
@@ -0,0 +1,17 @@
+﻿# http://www.pythonchallenge.com/pc/def/map.html
+
+STRING = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+text = ""
+
+for i in STRING:
+    text = text + (chr(ord(i)+2))
+           
+print(text)
+
+STRING = "map"
+text = ""
+
+for i in STRING:
+    text = text + (chr(ord(i)+2))
+         
+print(text)
---
+++ levels/pythonchallange_001/pythonchallange_001.rst
@@ -0,0 +1,10 @@
+﻿Level 1
+=======
+
+URL: http://www.pythonchallenge.com/pc/def/map.html
+
+.. image:: http://www.pythonchallenge.com/pc/def/map.jpg
+
+everybody thinks twice before solving this.
+
+g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.
