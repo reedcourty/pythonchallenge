7e708f1fb427cd4607dfaa817881a16fd50b17d4
Gyorgy Nadudvari <reedcourty@gmail.com>
2011-07-23 10:07:46 +0200

    Added the solution of Level 0.

---
+++ levels/pythonchallange_000/pythonchallange_000.py
@@ -0,0 +1,8 @@
+﻿# http://www.pythonchallenge.com/pc/def/0.html
+# Hint: try to change the URL address. 
+# On http://www.pythonchallenge.com/pc/def/1.html
+#   2**38 is much much larger.
+
+import webbrowser
+
+webbrowser.open('http://www.pythonchallenge.com/pc/def/' + str(2**38) + '.html')
---
+++ levels/pythonchallange_000/pythonchallange_000.rst
@@ -0,0 +1,8 @@
+﻿Level 0
+=======
+
+URL: http://www.pythonchallenge.com/pc/def/0.html
+
+.. image:: http://www.pythonchallenge.com/pc/def/calc.jpg
+
+Hint: try to change the URL address.
