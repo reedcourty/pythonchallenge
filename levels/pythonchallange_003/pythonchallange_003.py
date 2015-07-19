766b284831cee01d7e1e7f2b4251d9152c851077
reedcourty <reedcourty@gmail.com>
2015-07-19 18:28:48 +0200

    Added the solution of Level 3.

---
+++ levels/pythonchallange_003/pythonchallange_003.py
@@ -0,0 +1,36 @@
+﻿#!python
+# -*- coding: UTF-8 -*-
+
+# http://www.pythonchallenge.com/pc/def/equality.html
+
+import string
+
+import requests
+
+r = requests.get("http://www.pythonchallenge.com/pc/def/equality.html")
+
+full_source = r.text
+
+secret = full_source[full_source.rfind("<!--")+5:-6].replace("\n","")
+
+answer = ""
+b = ""
+there_is_more = True
+coord = 0
+
+for i in secret:
+    if i in string.ascii_lowercase:
+        b = b + '0'
+    else:
+        b = b + '1'
+
+while there_is_more:
+    coord = b.find('011101110', coord+1)
+
+    if (coord == -1):
+        there_is_more = False
+    else:
+        answer = answer + secret[coord+4]
+
+print(answer)
+
---
+++ levels/pythonchallange_003/pythonchallange_003.rst
@@ -0,0 +1,8 @@
+﻿Level 3
+=======
+
+URL: http://www.pythonchallenge.com/pc/def/equality.html
+
+.. image:: http://www.pythonchallenge.com/pc/def/bodyguard.jpg
+
+One small letter, surrounded by EXACTLY three big bodyguards on each of its sides.
