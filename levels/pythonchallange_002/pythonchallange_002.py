ceed7306e367eec3a8b8ecc4db26e053c05ef329
reedcourty <reedcourty@gmail.com>
2015-07-19 17:27:43 +0200

    Added the solution of Level 2.

---
+++ levels/pythonchallange_002/pythonchallange_002.py
@@ -0,0 +1,21 @@
+﻿#!python
+# -*- coding: UTF-8 -*-
+
+# http://www.pythonchallenge.com/pc/def/ocr.html
+
+import requests
+
+r = requests.get("http://www.pythonchallenge.com/pc/def/ocr.html")
+
+full_source = r.text
+
+secret = full_source[full_source.rfind("<!--")+5:-6]
+
+b = []
+answer = ""
+
+for i in secret:
+    if i not in answer:
+        answer = answer + i
+
+print(answer)
---
+++ levels/pythonchallange_002/pythonchallange_002.rst
@@ -0,0 +1,9 @@
+﻿Level 2
+=======
+
+URL: http://www.pythonchallenge.com/pc/def/ocr.html
+
+.. image:: http://www.pythonchallenge.com/pc/def/ocr.jpg
+
+recognize the characters. maybe they are in the book, but MAYBE they are in the page source.
+
